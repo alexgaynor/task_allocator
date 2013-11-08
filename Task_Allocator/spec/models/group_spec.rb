8de4c2bef95df1747342ea55f7becdb25d61b04b
ohsunmi <ohsun14@gmail.com>
2013-11-08 15:12:41 -0500

    tests

--- Task_Allocator/spec/models/group_spec.rb
+++ Task_Allocator/spec/models/group_spec.rb
@@ -1,8 +1,45 @@
 require 'spec_helper'
 
-# group_name, team_leader, home_address, group_type
-
+# group_name, group_description, home_address, group_type
 
 describe Group do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "has a valid group name" do
+  	group = Group.new
+  	group.group_name = "The Jones Family"
+  	group.save
+  	group.group_name.should == "The Jones Family"
+  end
+
+  it "has a valid group description" do
+  	group = Group.new
+  	group.group_description = "A loving family with 3 sons and an awesome tiger-like-striped dog."
+  	group.save
+  	group.group_description.should == "A loving family with 3 sons and an awesome tiger-like-striped dog."
+  end
+
+  it "has a valid home address" do
+  	group = Group.new
+  	group.home_address = "99 Cherry Lane, New York, NY 10017"
+  	group.save
+  	group.home_address.should == "99 Cherry Lane, New York, NY 10017"
+  end
+
+  it "has a valid group type" do
+  	group = Group.new
+  	group.group_type = "family"
+  	group.save
+  	group.group_type.should == "family"
+  end
+
+  it "has and belongs to many users" do
+		t = Group.reflect_on_association(:users)
+		t.macro.should == :has_and_belongs_to_many
+	end
+
+	it "has many tasks" do
+		t = Group.reflect_on_association(:tasks)
+		t.macro.should == :has_many
+	end
+
 end
--- Task_Allocator/spec/models/user_spec.rb
+++ Task_Allocator/spec/models/user_spec.rb
@@ -1,13 +1,73 @@
 require 'spec_helper'
 
-# users should have username, firstname, lastname, email, password, nickname, phone_num, prof_pic
-
-
+# username, firstname, lastname, nickname, email, password, phone_num, prof_pic
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "has a valid username" do
+  	user = User.new
+  	user.username = "Alex123"
+  	user.save
+  	user.username.should == "Alex123"
+	end
 
-  it "should have a username, firstname, lastname, "
+	it "has a valid first name" do
+		user = User.new
+		user.firstname = "Alex"
+		user.save
+		user.firstname.should == "Alex"
+	end
 
+	it "has a valid last name" do
+		user = User.new
+		user.lastname = "Jones"
+		user.save
+		user.lastname.should == "Jones"
+	end
+
+	it "has a valid nick name" do
+		user = User.new
+		user.nickname = "A-dawg"
+		user.save
+		user.nickname.should == "A-dawg"
+	end
+
+	it "has a valid email" do
+		user = User.new
+		user.email = "alex123@test.com"
+		user.save
+		user.email.should == "alex123@test.com"
+	end
+
+	it "has a valid password" do
+		user = User.new
+		uesr.password = "password"
+		user.save
+		user.password.should == "password"
+	end
+
+	it "has a valid phone number" do
+		user = User.new
+		user.phone_num = "(516)867-5309"
+		user.save
+		user.phone_num.should == "(516)867-5309"
+	end
+
+	it "has a valid profile picture url" do
+		user = User.new
+		user.prof_pic = "image.jpg"
+		user.save
+		user.prof_pic == "image.jpg"
+	end
+
+	it "has and belongs to many groups" do
+		t = User.reflect_on_association(:groups)
+		t.macro.should == :has_and_belongs_to_many
+	end
+
+	it "has many tasks" do
+		t = User.reflect_on_association(:tasks)
+		t.macro.should == :has_many
+	end
 
 end
