bed19edb3fa4dcd46893684f7970884970bdedc1
alexgaynor <agaynor72@gmail.com>
2013-11-08 10:23:58 -0500

    init commit

---
+++ Task_Allocator/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaskAllocator::Application.config.secret_key_base = 'ca6ddb76086179d63b56b1c6f385a624aad11ed0d83c31bd24ede95f08ff60f1b3d6725db9ac41def3164628c822fb346596307e18e7ea0f26f82a59f0cd69f0'
