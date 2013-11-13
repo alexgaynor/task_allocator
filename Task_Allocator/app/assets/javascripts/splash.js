56e60e726c8fba05dfb8c91ff517a3b03ce13e96
ohsunmi <ohsun14@gmail.com>
2013-11-13 01:02:17 -0500

    its late

--- Task_Allocator/app/assets/javascripts/splash.js
+++ Task_Allocator/app/assets/javascripts/splash.js
@@ -1,5 +1,5 @@
 var signInLink, signUpLink, signInForm, signUpForm;
-var $profile_info, $profile_pic, $user_container, $inputs;
+var $profile_info, $profile_pic, $user_container, $inputs, $user_wrap;
 var values = {};
 
 
@@ -8,7 +8,7 @@ function profileSlideUp() {
 }
 
 function profileSlideDown() {
-	$user_container.animate({ marginTop: 20 });
+	$user_container.animate({ marginTop: 20, marginBottom: 100 });
 }
 
 // when profile photo clicked
@@ -24,7 +24,8 @@ function photoClick() {
 
 // edit profile info
 function editProfileInfo() {
-	$(".profile_info div").on("click", function(e) {
+	$user_wrap.on("click", function(e) {
+		console.log(e.target);
 		all = $(".profile_info div");
 		all.each(function() {
 			var dataset = $(this);
@@ -32,26 +33,31 @@ function editProfileInfo() {
 			var newid = theid+"-form";
 			var currval = dataset.text();
 			dataset.empty();
-			$(this).unbind('click');
+			// $(this).unbind('click');
 			$('<input type="text" id="'+newid+'" value="'+currval+'" class="edit_field">').appendTo(dataset);
 		});
-	submitChanges();
+		keypress();
+	});
+}
+
+// keypress edit
+function keypress() {
+	$(".edit_field").on("keypress", function(e) {
+		submitChanges(e);
 	});
 }
 
 // submit profile edits
-function submitChanges() {
-	$(".edit_field").on("keypress", function(e) {
-		console.log(e);
-		$inputs.each(function() {
-			var key = $(this).parent().attr('id');
-			var value = $(this).val();
-			values[key] = value;
-		});
-		if(e.keyCode == 13) {
-			editProfile(values);
-		}
+function submitChanges(e) {
+	$inputs = $('.profile_info :input');
+	$inputs.each(function() {
+		var key = $(this).parent().attr('id');
+		var value = $(this).val();
+		values[key] = value;
 	});
+	if(e.keyCode == 13 || e.keyCode == undefined) {
+		editProfile(values);
+	}
 }
 
 // ajax update user attributes
@@ -62,7 +68,7 @@ function editProfile(values) {
 		url: "/users/update",
 		data: {user: values}
 	}).success(function() {
-		console.log('ajaxed');
+		profileSlideUp();
 	});
 }
 
@@ -80,6 +86,7 @@ $(function() {
 	$profile_pic = $("#profile_pic");
 	$user_container = $("#user-cont");
 	$inputs = $(".profile_info div :input");
+	$user_wrap = $("#user-wrap");
 
 	photoClick();
 	editProfileInfo();
@@ -105,11 +112,13 @@ $(function() {
 	});
 
 	// confirmation box for profile changes
-	$(document).mouseup(function(e) {
-		if (!$profile_info.is(e.target) && !($profile_info.children()).is(e.target) && !$(":input").is(e.target) && $user_container.css('marginTop') == '20px') {
+	$(':input').mouseup(function(e) {
+		e.stopPropagation();
+		console.log(e.target);
+		if (!$user_wrap.is(e.target) && !$(":input").is(e.target) && $user_container.css('marginTop') == '20px') {
 			var didConfirm = confirm("Save changes?");
 			if (didConfirm == true) {
-				console.log('save please');
+				submitChanges(e);
 				profileSlideUp();
 			} else {
 				profileSlideUp();
@@ -118,3 +127,4 @@ $(function() {
 	});
 	
 }); // end of on load
+
